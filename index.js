704c8a7d5446fa72eeb0e8d00612fd42ef88d168
GuiOrlando <guilherme160702@gmail.com>
2023-05-08 22:40:20 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,55 @@
+/*
+//Exercicio 01
+let contador = 0;
+for(let i = 0; i < 100; i++) {
+    contador++;
+    if(contador %2 == 0) {
+        console.log(contador);
+    }
+}
+*/
+
+/*
+//Exercicio 02
+const numeros = [2, 4, 6, 8]
+let soma = 0;
+
+for(let i = 0; i < numeros.length; i++) {
+    soma = soma + numeros[i];
+}
+
+console.log("Soma = " + soma);
+*/
+
+/*
+//Exercicio 03
+function main() {
+    const numeros = [-1, 1, -2, 2, -3, 3]
+    let maiores;
+
+    for(let i = 0; i < numeros.length; i++){
+        if(numeros[i] >= 0) {
+            maiores = numeros[i]
+            console.log(maiores)
+        }
+    }
+}
+
+main();
+*/
+
+/*
+//Exercicio 04
+const numeros = [1, 2, 3, 4, 5]
+
+for(let i = 0; i < numeros.length; i++) {
+    console.log(numeros[i])
+}
+
+console.log("\n")
+
+for(let i = 0; i < numeros.length; i++) {
+    numeros[i] = numeros[i] *2
+    console.log(numeros[i])
+}
+*/
